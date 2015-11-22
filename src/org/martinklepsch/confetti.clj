e0824226970b298778ed87e482d34530f5b6d7d9
Martin Klepsch <martinklepsch@googlemail.com>
2015-11-22 20:36:52 +0100

    add stack event reporting

--- src/org/martinklepsch/confetti.clj
+++ src/org/martinklepsch/confetti.clj
@@ -1,79 +1,96 @@
 (ns org.martinklepsch.confetti
-  (:require [org.martinklepsch.confetti.cloudfront :as cf]
-            [org.martinklepsch.confetti.route53 :as r53]
-            [org.martinklepsch.confetti.s3 :as s3]
-            ;[amazonica.aws.route53 :as r53]
-            [amazonica.aws.identitymanagement :as iam]
-            [clojure.pprint   :refer [pprint]]
+  (:require [org.martinklepsch.confetti.cloudformation :as cf]
+            [org.martinklepsch.confetti.util :as util]
+            [camel-snake-kebab.core :as case]
+            [clojure.pprint :refer [pprint]]
             [clojure.data.json :as json]
             [clojure.tools.logging :as log]))
 
-;; AmazonAWS DNS Servers:
-;; DNS1: ns-1769.awsdns-29.co.uk
-;; DNS2: ns-267.awsdns-33.com
-;; DNS3: ns-993.awsdns-60.net
-;; DNS4: ns-1090.awsdns-08.org
+;; TODO make sure future is retrieved/deref'd at some point to make any exceptions bubble up
+;; The stuff below might not be needed anymore if this works properly
+(Thread/setDefaultUncaughtExceptionHandler
+ (reify Thread$UncaughtExceptionHandler
+   (uncaughtException [_ thread ex]
+     (println ex "Uncaught exception on" (.getName thread)))))
 
-;; Validation
-(def devcred (read-string (slurp "aws-cred.edn")))
+(defmulti print-event (comp case/->kebab-case-keyword :resource-status))
 
-(defn create-project [cred root-domain]
-  (log/info "Create bucket...")
-  (log/info (pr-str (s3/create-buckets cred root-domain)))
-  (log/info "Create HostedZone...")
-  (log/info (pr-str (r53/create-hosted-zone cred root-domain)))
-  (log/info "Add Record sets to HostedZone...")
-  (log/info (pr-str (r53/add-record-sets cred root-domain)))
-  (log/info "Create Cloudfront Distribution")
-  (log/info (pr-str (cf/create-distribution cred root-domain))))
+(defmethod print-event :default [ev]
+  (println "DEFAULT EVENT PRINTER")
+  (println ev))
 
-;; (create-project devcred "cljs.io")
+(defmethod print-event :create-complete [ev]
+  ;; (printf "[%s] %s\n" (:resource-status ev) (:resource-type ev))
+  (println "[" (:resource-status ev) "]" (:resource-type ev)))
 
-;; (defn delete-project [cred root-domain]
-;;   nil)
+(defmethod print-event :create-in-progress [ev]
+  ;; `printf` behaves strange in non main threads??
+  ;; (println (System/currentTimeMillis))
+  (println "[" (:resource-status ev) "]" (:resource-type ev)))
 
-;; (defn username-available? [cred name]
-;;   (not ((set (map :user-name (:users (iam/list-users cred)))) name)))
+(def stack-events (atom {}))
+
+(defn report-events
+  ([stack-id done-promise]
+   (report-events stack-id done-promise false))
+  ([stack-id done-promise verbose?]
+   (let [;;events (cf/get-events stack-id)
+         events (get-events-stub stack-id)
+         new    (reduce
+                 (fn [m {:keys [event-id] :as ev}]
+                   (if (get m event-id)
+                     m
+                     (do
+                       (print-event ev)
+                       (when verbose? (println ev))
+                       (assoc m event-id ev))))
+                 @stack-events
+                 events)]
+     (reset! stack-events new)
+     (when (cf/succeeded? events)
+       (deliver done-promise true)))))
+
+(defn report-stack-events [stack-id opts]
+  (let [done  (promise)
+        report #(report-events stack-id done true)
+        sched  (util/schedule report 300)]
+    ;; TODO if success? -> error handling!
+    (when @done
+      (future-cancel sched))))
 
 (comment
-  (process-tx cred {:aws/model :x})
-  (map #(process-tx cred %) (map reverse-tx (gen-transactions "xxxxxxxxxx.com")))
-  (map reverse-tx (gen-transactions "xxxxxxxxxx.com"))
+  (defn rand-str []
+    (->> (fn [] (rand-nth ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k"]))
+         repeatedly
+         (take 5)
+         (apply str)))
 
-  (create-buckets cred "cljs.io" {})
-  (delete-buckets cred "cljs.io" {})
-  (s3/does-bucket-exist cred "sdd87dfv8fv7v87ads8s")
-  (s3/does-bucket-exist cred "cljs.io")
-  (not-any? true? [false false])
-  (buckets-available? cred "sdd87dfv8fv7v87ads8s" "mxxxxxxjhdsfsdf98gfdfsdf3")
+  (let [s   (rand-str)
+        ran (cf/run-template
+             (str "static-site-" s)
+             (cf/template {:dns? false})
+             {:user-domain (str s ".martinklepsch.org")})]
+    (println ran)
+    (report-stack-events (:stack-id ran)))
 
-  (s3/set-bucket-website-configuration
-   cred
-   :bucket-name "skinnnn"
-   :configuration {;:index-document-suffix "index.html"
-                                        ;:error-document (or nil "")
-                   })
-  (s3/get-bucket-website-configuration cred "skinnnn")
+  (do
+    (reset! evs [])
+    (reset! stack-events {})
+    (report-stack-events "x" {}))
 
-  (s3/set-bucket-website-configuration
-   cred
-   :bucket-name "skinnnn"
-   :configuration {:index-document-suffix "index.html"
-                                        ;:error-document (or nil "")
-                   })
-  (s3/get-bucket-website-configuration cred "www.martinklepsch.org") 
+  (do ;; Events testing
+    (def evs (atom []))
 
-  (does-user-exist cred pn)
+    (defn get-events-stub [_]
+      (Thread/sleep 300)
+      (swap! evs conj
+             (if (< (count @evs) 10)
+               {:event-id (gensym) :resource-status "CREATE_IN_PROGRESS" :resource-type "AWS::S3::Bucket" :fake-event "yes"}
+               {:event-id (gensym) :resource-status "CREATE_COMPLETE" :resource-type "AWS::CloudFormation::Stack"}))))
 
-  (def pn "my-project-1md0l4mb3")
-  (s3/create-bucket pn)
-  (s3/delete-bucket pn)
-  (iam/create-user cred :user-name pn)
-  (iam/delete-user cred :user-name pn)
-  (iam/put-user-policy :user-name pn
-                       :policy-name (str pn "-S3FullAccess")
-                       :policy-document (json/write-str (policy pn)))
-  (iam/create-access-key :user-name pn)
-  (count (map :user-name (:users (iam/list-users cred))))
-  (map :name (s3/list-buckets cred)))
+  (def p   (promise))
+  (def ea  (events-reporter "x" p))
+  (def fut (util/schedule ea 2))
+  (def fut (util/schedule (fn [] (println "x")) 2))
 
+  )
