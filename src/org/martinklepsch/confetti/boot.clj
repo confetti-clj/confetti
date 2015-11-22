4a0a8bb1501e883606ac54b853666d691a9c300c
Martin Klepsch <martinklepsch@googlemail.com>
2015-11-22 20:46:52 +0100

    add dry-run boot task option

--- src/org/martinklepsch/confetti/boot.clj
+++ src/org/martinklepsch/confetti/boot.clj
@@ -1,18 +1,23 @@
 (ns org.martinklepsch.confetti.boot
   (:require [org.martinklepsch.confetti :as confetti]
             [org.martinklepsch.confetti.cloudformation :as cf]
+            [clojure.pprint :as pp]
             [boot.core :as b]))
 
 (b/deftask confetti
   "Create all resources for ideal deployment of static sites"
   [n dns?          boolean "Handle DNS? (i.e. create Route53 Hosted Zone)"
-   d domain DOMAIN str     "domain of the future site (without protocol)"]
+   d domain DOMAIN str     "Domain of the future site (without protocol)"
+   r dry-run?      boolean "Only print to be ran template, don't run it"]
   (c/with-pre-wrap fs
     (assert domain "Domain is required!")
-    (let [ran (cf/run-template
-               (str "static-site-" domain)
-               (cf/template {:dns? dns?})
-               {:user-domain domain})]
-      (println ran)
-      (confetti/report-stack-events (:stack-id ran))
+    (let [tpl (cf/template {:dns? dns?})
+          stn (str "confetti-static-site" domain)
+          ran (when-not dry-run?
+                (cf/run-template stn tpl {:user-domain domain}))]
+      (if dry-run?
+        (pp/pprint tpl)
+        (do
+          (println ran)
+          (confetti/report-stack-events (:stack-id ran))))
       fs)))
