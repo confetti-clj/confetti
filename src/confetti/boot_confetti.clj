948addafb84cff42733fba7654b9fb2a8f145311
Martin Klepsch <martinklepsch@googlemail.com>
2016-11-26 16:18:44 +0700

    allow direct usage of confetti-edn to specify options; improve errors

--- src/confetti/boot_confetti.clj
+++ src/confetti/boot_confetti.clj
@@ -3,6 +3,7 @@
   (:require [confetti.serialize :refer [->str]]
             [clojure.string :as string]
             [clojure.java.io :as io]
+            [clojure.edn :as edn]
             [clojure.pprint :as pp]
             [boot.pod :as pod]
             [boot.util :as u]
@@ -25,6 +26,12 @@
     (require 'confetti.serialize)
     (require 'confetti.report))
   cpod)
+
+(defn assert-exit [assert-success? & msgs]
+  (when-not assert-success?
+    (u/exit-error
+     (doseq [msg msgs]
+       (u/fail (str msg "\n\n"))))))
 
 ;; (let [verbose true]
 ;;   (pod/with-call-in cpod
@@ -60,12 +67,12 @@
    d domain DOMAIN str  "Domain of the future site (without protocol)"
    r dry-run       bool "Only print to be ran template, don't run it"]
   (b/with-pre-wrap fs
-    (assert domain "Domain is required!")
-    (assert access-key "Access Key is required!")
-    (assert secret-key "Secret Key is required!")
+    (assert-exit domain "The :domain option of the create-site task is required!")
+    (assert-exit access-key "The :access-key option of the create-site task is required!")
+    (assert-exit secret-key "The :secret-key option of the create-site task is required!")
     (let [cpod (prep-pod (confetti-pod))]
       (when (pod/with-call-in cpod (confetti.util/root-domain? ~domain))
-        (assert dns "Root domain setups must enable `dns` option"))
+        (assert-exit dns "Root domain setups must enable the :dns option"))
       (let [creds {:access-key access-key :secret-key secret-key}
             tpl (pod/with-eval-in cpod
                   (confetti.cloudformation/template {:dns? ~dns}))
@@ -118,10 +125,10 @@
                            (.listFiles)
                            (remove #(.isDirectory %))
                            (filter #(.endsWith (.getName %) ".confetti.edn"))
-                           (remove (comp :cloudfront-url read-string slurp)))]
+                           (remove (comp :cloudfront-url edn/read-string slurp)))]
       (doseq [p preliminary]
         (u/info "Fetching outputs for %s\n" (.getName p))
-        (let [stack-id (-> p slurp read-string :stack-id)
+        (let [stack-id (-> p slurp edn/read-string :stack-id)
               outputs  (pod/with-eval-in cpod
                          (try
                            (confetti.cloudformation/get-outputs ~creds ~stack-id)
@@ -134,8 +141,13 @@
   (mapv (fn [tf] {:s3-key (:path tf) :file (b/tmp-file tf)})
         (b/output-files fs)))
 
+(defn read-confetti-edn [id]
+  (let [f (io/file (str id ".confetti.edn"))]
+    (assert-exit (.exists f) (str "The file " (.getName f) " could not be found!"))
+    (-> f slurp edn/read-string)))
+
 (b/deftask sync-bucket
-  "Sync fileset (default), directory or selected files to S3 bucket.
+  "Sync fileset (default & easiest), directory or selected files to S3 bucket.
 
    Use the `dir` option to specify a directory to sync. To upload only selected
    files or attach special metadata use `file-maps` or `file-maps-path` options.
@@ -148,10 +160,12 @@
    or a path pointing to any other file on your filesystem (with leading /).
 
    Other options:
+   - `confetti-edn` if provided this file is used to specify the bucket, credentials and cloudfront-id options
    - `dry-run` will cause all S3 related side effects to be skipped
    - `prune` will cause S3 objects which are not supplied as file-maps to be
      deleted from the target S3 bucket"
-  [b bucket         BUCKET str  "Name of S3 bucket to push files to"
+  [e confetti-edn   PATH   str  "The name of a .confetti.edn file in the current working directory"
+   b bucket         BUCKET str  "Name of S3 bucket to push files to"
    a access-key     ACCESS str  "AWS access key to use"
    s secret-key     SECRET str  "AWS secret key to use"
    d dir            DIR    str  "Directory to sync as is"
@@ -161,43 +175,59 @@
    p prune                 bool "Delete files from S3 bucket not in fileset/dir"
    c cloudfront-id  DIST   str  "CloudFront Distribution ID to create invalidation (optional)"]
   (b/with-pre-wrap fs
-    (assert bucket "A bucket name is required!")
-    (assert access-key "Access Key is required!")
-    (assert secret-key "Secret Key is required!")
-    (newline)
-    (let [creds {:access-key access-key :secret-key secret-key}
-          cpod  (prep-pod (confetti-pod))
-          ;; Read file-maps from the various possible sources
-          fmaps (cond
-                  file-maps      file-maps
-                  file-maps-path (read-string (slurp (b/tmp-file (get-in fs [:tree file-maps-path]))))
-                  dir            (pod/with-eval-in cpod
-                                   (-> (clojure.java.io/file ~dir)
-                                       confetti.s3-deploy/dir->file-maps
-                                       confetti.serialize/->str))
-                  :else          (fileset->file-maps fs))
-          ;; If relative paths are supplied lookup files in fileset
-          fmaps* (mapv (fn [{:keys [file] :as fm}]
-                         (if (and (string? file) (not (.startsWith file "/")))
-                           (assoc fm :file (b/tmp-file (get-in fs [:tree file])))
-                           fm))
-                       fmaps)
-          results (pod/with-eval-in cpod
-                    (confetti.s3-deploy/sync!
-                     ~creds ~bucket (confetti.serialize/->file ~(pod/send! (->str fmaps*)))
-                     {:dry-run? ~dry-run :prune? ~prune :report-fn (resolve 'confetti.report/s3-report)}))]
-      (let [{:keys [uploaded updated unchanged deleted]} results]
-        (when (< 0 (max (count uploaded) (count updated) (count deleted)))
-          (newline))
-        (let [paths (mapv #(str "/" %) (concat uploaded updated deleted))]
-          (when (and cloudfront-id (seq paths) (not dry-run))
-            (u/info "Creating CloudFront invalidation for %s paths.\n" (count paths))
-            (u/dbug "Paths: %s\n" (pr-str paths))
-            (pod/with-eval-in cpod
-              (confetti.s3-deploy/cloudfront-invalidate! ~creds ~cloudfront-id ~paths)))))
-      (u/info "%s new files uploaded.\n" (-> results :uploaded count))
-      (u/info "%s existing files updated. %s unchanged.\n"
-              (-> results :updated count)
-              (-> results :unchanged count))
-      (u/info "%s files deleted.\n" (-> results :deleted count)))
+    (let [cedn (read-confetti-edn confetti-edn)]
+      ;; Various checking / error handling
+      (if confetti-edn
+        (do
+          (assert-exit (:bucket-name cedn) "You supplied the confetti-edn option but it does not contain a :bucket-name key")
+          (assert-exit (:secret-key cedn) "You supplied the confetti-edn option but it does not contain a :access-key key")
+          (assert-exit (:access-key cedn) "You supplied the confetti-edn option but it does not contain a :secret-key key")
+          (assert-exit (and (not bucket) (not cloudfront-id) (not access-key) (not secret-key))
+                  "When supplying the confetti-edn option, don't provide the bucket, cloudfront-id, access-key or secret-key options"))
+        (do
+          (assert-exit bucket "Bucket option is required!")
+          (assert-exit access-key "Access Key option is required!")
+          (assert-exit secret-key "Secret Key option is required!")))
+      (newline)
+
+      (let [bucket (if cedn (:bucket-name cedn) bucket)
+            cf-id  (if cedn (:cloudfront-id cedn) cloudfront-id)
+            creds  (if cedn
+                     (select-keys cedn [:access-key :secret-key])
+                     {:access-key access-key :secret-key secret-key})
+            ;; Create pod and require needed namespaces
+            cpod   (prep-pod (confetti-pod))
+            ;; Read file-maps from the various possible sources
+            fmaps  (cond
+                     file-maps      file-maps
+                     file-maps-path (edn/read-string (slurp (b/tmp-file (get-in fs [:tree file-maps-path]))))
+                     dir            (pod/with-eval-in cpod
+                                      (-> (clojure.java.io/file ~dir)
+                                          confetti.s3-deploy/dir->file-maps
+                                          confetti.serialize/->str))
+                     :else          (fileset->file-maps fs))
+            ;; If relative paths are supplied lookup files in fileset
+            fmaps* (mapv (fn [{:keys [file] :as fm}]
+                           (if (and (string? file) (not (.startsWith file "/")))
+                             (assoc fm :file (b/tmp-file (get-in fs [:tree file])))
+                             fm))
+                         fmaps)
+            results (pod/with-eval-in cpod
+                      (confetti.s3-deploy/sync!
+                       ~creds ~bucket (confetti.serialize/->file ~(pod/send! (->str fmaps*)))
+                       {:dry-run? ~dry-run :prune? ~prune :report-fn (resolve 'confetti.report/s3-report)}))]
+        (let [{:keys [uploaded updated unchanged deleted]} results]
+          (when (< 0 (max (count uploaded) (count updated) (count deleted)))
+            (newline))
+          (let [paths (mapv #(str "/" %) (concat uploaded updated deleted))]
+            (when (and cf-id (seq paths) (not dry-run))
+              (u/info "Creating CloudFront invalidation for %s paths.\n" (count paths))
+              (u/dbug "Paths: %s\n" (pr-str paths))
+              (pod/with-eval-in cpod
+                (confetti.s3-deploy/cloudfront-invalidate! ~creds ~cf-id ~paths)))))
+        (u/info "%s new files uploaded.\n" (-> results :uploaded count))
+        (u/info "%s existing files updated. %s unchanged.\n"
+                (-> results :updated count)
+                (-> results :unchanged count))
+        (u/info "%s files deleted.\n" (-> results :deleted count))))
     fs))
